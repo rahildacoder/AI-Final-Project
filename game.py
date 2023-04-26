4b8b8afdb54aacdf023bf5b12d5ad0877a7f8800
rahildacoder <rahilbadkul@gmail.com>
2023-04-26 02:25:43 -0400

    implemented monte carlo tree search

--- game.py
+++ game.py
@@ -8,9 +8,9 @@ ROWS = 6
 # number of columns on the board
 COLUMNS = 7
 # player's turn number
-PLAYER = 0
+PLAYER = 1
 # AI's turn number
-AI = 1
+AI = 2
 # empty spot on board
 EMPTY = 0
 # represents the player's piece on the board
@@ -22,7 +22,9 @@ WINDOW_LENGTH = 4
 # the number of nodes traversed in a move
 NODES = 0
 # the depth for the minimax
-DEPTH = 10
+DEPTH = 100
+# the time per AI move
+TIME = 5
 
 # creates the initial board
 def createBoard():
@@ -34,7 +36,7 @@ def drop(board, row, col, piece):
     board[row][col] = piece
 
 # determines whether a column is full
-def validLocation(board, col):
+def validColumn(board, col):
     return board[ROWS - 1][col] == 0
 
 # determines the next open row in a column
@@ -135,11 +137,11 @@ def scorePos(board, piece):
 
 # determines whether a certain move is terminal
 def isTerminal(board):
-    return win(board, PLAYER_PIECE) or win(board, AI_PIECE) or len(getValidLocations(board)) == 0
+    return win(board, PLAYER_PIECE) or win(board, AI_PIECE) or len(getValidColumns(board)) == 0
 
 # minimax function
 def minimax(board, depth, alpha, beta, maxPlayer):
-    valid = getValidLocations(board)
+    valid = getValidColumns(board)
     terminal = isTerminal(board)
     if depth == 0 or terminal:
         global NODES
@@ -189,12 +191,226 @@ def minimax(board, depth, alpha, beta, maxPlayer):
         return bestCol, currScore
 
 # determines all valid columns on a board
-def getValidLocations(board):
+def getValidColumns(board):
     valid = []
     for c in range(COLUMNS):
-        if validLocation(board, c):
+        if validColumn(board, c):
             valid.append(c)
     return valid
+
+# Node class for MCTS
+class Node:
+
+    # Class initialization
+    def __init__(self, parent, board, turn):
+        # Parent node
+        self.parent = parent
+        # Board state of node
+        self.board = board
+        # Flip turn
+        if turn == PLAYER:
+            self.turn = AI
+        else:
+            self.turn = PLAYER
+        # Total outcomes
+        self.outcomes = 0 
+        # Number of visits
+        self.visits = 0 
+        # Undiscovered children
+        self.children = []
+        # Not yet expanded
+        self.expanded = False
+        # Terminal node or not
+        self.terminal = isTerminal(self.board)
+
+        # Add child to node
+    def addChild(self):
+        # node already expanded
+        if self.expanded:
+            return
+        # get board of every child
+        childBoard = list()
+        for child in self.children:
+            childBoard.append(child.board)
+        # find new child
+        valid = getValidColumns(self.board)
+        # For all valid columns
+        for c in valid:
+            for r in range(ROWS): # Not using nextOpenRow method because it gave issues
+                if self.board[r, c] == 0:
+                    boardCopy = self.board.copy()
+                    if self.turn == PLAYER:
+                        drop(boardCopy, r, c, AI) # create child state
+                        if childBoard:
+                            if not self.compare(boardCopy, childBoard):
+                                self.children.append(Node(self, boardCopy, PLAYER)) # add child node
+                                return
+                            else:
+                                break
+                        else:
+                            self.children.append(Node(self, boardCopy, PLAYER))
+                            return
+                    else:
+                        drop(boardCopy, r, c, PLAYER)
+                        if childBoard:
+                            if not self.compare(boardCopy, childBoard):
+                                self.children.append(Node(self, boardCopy, AI))
+                                return
+                            else:
+                                break
+                        else:
+                            self.children.append(Node(self, boardCopy, AI))
+                            return
+        # no children found
+        self.expanded = True
+        return
+
+    # Checks if states are equal
+    def compare(self, new, children):
+        for c in children:
+            if (new == c).all():
+                return True
+        return False
+
+# Checks if all of a node's children are visited
+def fullyExpanded(node):
+    visited = True
+    if len(getValidColumns(node.board)) == len(node.children):
+        for child in node.children:
+            if child.visits == 0:
+                visited = False
+        return visited
+    else:
+        return False
+    
+# Updates the number of visits and outcomes of each node
+def backpropagate(node, outcome):
+    # add when AI wins
+    if node.turn == AI_PIECE:
+        node.outcomes += outcome
+    # subtract when AI loses
+    else:
+        node.outcomes -= outcome
+    node.visits += 1
+    # stop at the top of the tree
+    if node.parent is None:
+        return
+    else:
+        backpropagate(node.parent, outcome)
+
+# Simulates moves from a given node and returns outcome
+def rollout(node):
+    board = node.board
+    turn = node.turn
+    if not node.terminal:
+        while(True):
+            # flip turn
+            if turn == 1:
+                turn = 2
+            else:
+                turn = 1
+            moves = getMoves(board, turn) # possible board positions from node
+            if moves:
+                board = random.choice(moves) # randomly selects move
+                if (win(board, AI_PIECE)):
+                    return 1
+                elif (win(board, PLAYER_PIECE)):
+                    return -1
+            # no moves left
+            else:
+                return calcOutcome(board)
+    # node is terminal
+    else:
+        return calcOutcome(board)
+    
+# Return outcome
+def calcOutcome(board):
+    if (win(board, AI_PIECE)):
+        return 1
+    elif (win(board, PLAYER_PIECE)):
+        return -1
+    else:
+        return 0
+    
+# Returns possible moves from board given turn
+def getMoves(board, turn):
+    moves = list()
+    valid = getValidColumns(board)
+    for c in valid:
+        r = nextOpenRow(board, c)
+        boardCopy = board.copy()
+        if turn == AI:
+            drop(boardCopy, r, c, PLAYER_PIECE)
+        else:
+            drop(boardCopy, r, c, AI_PIECE)
+        moves.append(boardCopy)
+        break
+    return moves
+
+# Chooses an unexplored child
+def pick(children):
+    for child in children:
+        if child.visits == 0:
+            return child
+        
+# Return the child with best UCT value
+def selectUCT(node):
+    bestUCT = -10000000
+    best_node = None
+    for child in node.children:
+        uct = (child.outcomes/child.visits) + 2*math.sqrt((math.log(node.visits))/child.visits)
+        if uct > bestUCT:
+            bestUCT = uct
+            best_node = child
+    if best_node is None:
+        return node
+    else:
+        return best_node
+
+# Selection of nodes 
+def select(node):
+    while(fullyExpanded(node)):
+        new = selectUCT(node)
+        if new == node:
+            break
+        else:
+            node = new
+    if node.terminal:
+        return node
+    else:
+        node.addChild()
+        global NODES
+        NODES += 1
+        if node.children:
+            return pick(node.children)
+        else:
+            return node
+
+# Runs the monte carlo algorithm
+def monteCarlo(root):
+    start = time.time()
+    while(time.time() - start) < TIME:
+        # selection and expansion
+        leaf = select(root)
+        # simulation
+        result = rollout(leaf)
+        # backpropogation
+        backpropagate(leaf, result)
+
+    return bestMove(root)
+            
+# Returns best move
+def bestMove(node):
+    maxVisit = 0
+    best = None
+    for child in node.children:
+        if child.visits > maxVisit:
+            maxVisit = child.visits
+            best = child
+    for r in range(ROWS):
+        for c in range(COLUMNS):
+            if best.board[r][c] != node.board[r][c]:
+                return c
 
 board = createBoard()
 gameOver = False
@@ -209,11 +425,13 @@ while not gameOver:
         col = int(input("Player 1 Make your Selection (0-6):"))
 
         # processes a turn
-        if validLocation(board, col):
+        if validColumn(board, col):
             row = nextOpenRow(board, col)
             drop(board, row, col, PLAYER_PIECE)
-            turn += 1
-            turn = turn % 2
+            if turn == AI:
+                turn = PLAYER
+            else:
+                turn = AI
             
             # processes a winning move
             if win(board, PLAYER_PIECE):
@@ -224,23 +442,38 @@ while not gameOver:
 
     # AI's turn
     if turn == AI:
+        # nodes explored in a turn
         NODES = 0
+        # column determined as best move
         col = 0
+        # minimax score of placing the piece in column
         minimaxScore = 0
-        # col = bestMove(board, AI_PIECE)
-        start = time.time()
-        for i in range(DEPTH):
-            end = time.time()
-            if (end - start < 5):
-                col, minimaxScore = minimax(board, i, -math.inf, math.inf, True)
+        # max depth explored in a turn
+        maxDepth = 0
+
+        # # timer start
+        # start = time.time()
+        # # iterates depth until time limit passes
+        # for i in range(DEPTH):
+        #     end = time.time()
+        #     if (end - start < TIME):
+        #         col, minimaxScore = minimax(board, i, -math.inf, math.inf, True)
+        #         maxDepth = i
+        # print("Max depth: " + str(maxDepth))
+        
+        # initialiaze root node
+        root = Node(None, board, AI)
+        col = monteCarlo(root)
         print("Total nodes searched: " + str(NODES))
 
         # processes a turn 
-        if validLocation(board, col):
+        if validColumn(board, col):
             row = nextOpenRow(board, col)
             drop(board, row, col, AI_PIECE)
-            turn += 1
-            turn = turn % 2
+            if turn == AI:
+                turn = PLAYER
+            else:
+                turn = AI
 
             # processes a winning move
             if win(board, AI_PIECE):
